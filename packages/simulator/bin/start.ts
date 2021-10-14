c4b3c323461cf7694ac5aaff02f4bbcae8a2aedb
Paul Cowan <paul.cowan@cutting.scot>
2021-10-14 07:37:54 +0100

    remove simulator

--- packages/simulator/bin/start.ts
+++
@@ -1,8 +0,0 @@
-import { main } from 'effection';
-import { createAcmecorpSimulationServer, Server } from '../src/server';
-
-main(function*() {
-  let server: Server = yield createAcmecorpSimulationServer();
-  console.log(`🚀 simulacrum running at http://localhost:${server.address.port}`);
-  yield;
-});
